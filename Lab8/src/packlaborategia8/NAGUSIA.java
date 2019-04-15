e8ad1abcc18490160331c158f2a4efb9605954a7
Martín Amézola <Martn Amzola@LAPTOP-PAPUI2UK>
2019-04-15 12:43:11 +0200

    Cambios en NAGUSIA

--- Lab8/src/packlaborategia8/NAGUSIA.java
+++ Lab8/src/packlaborategia8/NAGUSIA.java
@@ -13,16 +13,27 @@ public class NAGUSIA {
 		lib1 = new Liburua ("El principe de las tinieblas","Gorka",1111);
 		lib2 = new Liburua ("Apexero","Martin",2222);
 		lib3 = new Liburua ("AhoBizi","Toni",3333);
-		lib4 = new Liburua ("El prince","Garni",4444);
+		lib4 = new Liburua ("El prince","Garni",1111); /* Liburu hauu id berdina du lib1-rekin, 
+		horregatik, katalogatzean errore emango du eta liburuarenIdAldatu() metodoa saiatu beharko
+		dugu. */
 
-		erab1 = new Erabiltzailea("Ramon Gonzalez", 1234);
-		erab2 = new Erabiltzailea("Ramon Gonzalez", 1234);
-		erab3= new Erabiltzailea("Ramon Gonzalez", 1111);
-		erab4 = new Erabiltzailea("Ramon Gonzalez", 1234);
+		erab1 = new Erabiltzailea("Ramon Gonzalez", 1000);
+		erab2 = new Erabiltzailea("Jorge Estebanez", 2000);// jola
+		erab3= new Erabiltzailea("Dari Goitia", 3000);
+		erab4 = new Erabiltzailea("Pepe el de las Papas", 4000);
 
 		
 		kat = Katalogoa.getKatalogoa();
-		kat.mailegatuLiburua(lib1, pIdErabiltzailea);
+		kat.katalogatuLiburua(lib1);
+		kat.katalogatuLiburua(lib2);
+		kat.katalogatuLiburua(lib3); // Meto los tres libros en el Katalogoa.
+		
+		
+//		kat.mailegatuLiburua(1111, 1000);
+
+		
+
+		
 		
 		
 
